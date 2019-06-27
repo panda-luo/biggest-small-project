97bdfbbe2153478855f878d881c843819d57249f
panda <panda.luo@anker.com>
2019-06-27 17:00:32 +0800

    test push

--- src/main/java/club/panda1024/biggestsmallproject/web/frontend/DummyController.java
+++ src/main/java/club/panda1024/biggestsmallproject/web/frontend/DummyController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ *  this is a dummy controller
+ *
  * @author Panda [cristopanda@gmail.com]
  * @since 2019-06-27T14:57
  */
