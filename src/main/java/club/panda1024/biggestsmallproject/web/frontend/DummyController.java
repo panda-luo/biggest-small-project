8cc0f1b6b6a552e5775d7641920f2820be22403f
panda <panda.luo@anker.com>
2019-06-27 15:08:14 +0800

    init repo

---
+++ src/main/java/club/panda1024/biggestsmallproject/web/frontend/DummyController.java
@@ -0,0 +1,17 @@
+package club.panda1024.biggestsmallproject.web.frontend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Panda [cristopanda@gmail.com]
+ * @since 2019-06-27T14:57
+ */
+@RestController
+public class DummyController {
+
+  @GetMapping("dummy")
+  public String dummy() {
+    return "dummy";
+  }
+}
