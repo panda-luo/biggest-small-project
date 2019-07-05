4ad8ca87dac225ba9dc3bb5ce65ef9b88344d722
panda <panda.luo@anker.com>
2019-07-05 15:24:51 +0800

    feat#增加首页显示

--- src/main/java/club/panda1024/biggestsmallproject/site/web/controller/SiteRouter.java
+++ src/main/java/club/panda1024/biggestsmallproject/site/web/controller/SiteRouter.java
@@ -1,8 +1,11 @@
 package club.panda1024.biggestsmallproject.site.web.controller;
 
+import club.panda1024.biggestsmallproject.service.BgService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
 
 /**
  * @author Panda [cristopanda@gmail.com]
@@ -11,9 +14,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SiteRouter {
 
+  @Resource
+  private BgService bgService;
+
+
   @GetMapping(value = {"", "/", "/index"})
   public String index(Model model) {
+    initBg(model);
     return "site/index";
   }
+
+  /**
+   *  初始化index背景数据
+   */
+  private void initBg(Model model){
+    model.addAttribute("bg", bgService.getImageByBingJson());
+
+  }
+
 
 }
