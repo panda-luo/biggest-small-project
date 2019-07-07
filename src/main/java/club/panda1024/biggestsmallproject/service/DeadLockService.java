777a89265021d0e3f3f97654e8d55671dbe6abb5
panda <21181470@qq.com>
2019-07-07 22:40:05 +0800

    blog

--- src/main/java/club/panda1024/biggestsmallproject/service/DeadLockService.java
+++
@@ -1,9 +0,0 @@
-package club.panda1024.biggestsmallproject.service;
-
-/**
- * @author Panda [cristopanda@gmail.com]
- * @since 2019-07-06T16:25
- */
-public interface DeadLockService {
-  void locked();
-}
